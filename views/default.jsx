f5298fc76776198c2718815542dfe9abbf63dc7f
AndreTaq <andretaqian@gmail.com>
2023-06-02 20:24:02 -0700

    Adding default view for layout

---
+++ views/default.jsx
@@ -0,0 +1,16 @@
+const React = require('react')
+
+function Def (html) {
+    return(
+        <html>
+            <head>
+                <title>Title</title>
+            </head>
+            <body>
+                
+            </body>
+        </html>
+    )
+}
+
+module.express = Def
