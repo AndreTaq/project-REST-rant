6544b363d32ab343b683f50a6719473fb3223648
AndreTaq <andretaqian@gmail.com>
2023-06-03 11:44:37 -0700

    Adding styles to REST-Rant

--- index.js
+++ index.js
@@ -2,16 +2,21 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
-app.set('view engine', 'jsx')
-app.engine('jsx', require('express-react-views').createEngine())
-
-app.use('/places', require('./controllers/places'))
-
+// ROUTES
 app.get('/', (req, res) => {
     // res.send('Hello World!') changed to
     res.render('home')
 })
+app.use('/places', require('./controllers/places'))
 
+// Middleware
+// app.set('views' __dirname + '/views') come back to this later
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+
+
+// 404 Page
 app.get('*', (req, res) => {
     // res.status(404).send('<h1>404 Page</h1>') changed to
     res.render('error404')
--- views/default.jsx
+++ views/default.jsx
@@ -5,6 +5,8 @@ function Def (html) {
         <html>
             <head>
                 <title>Title</title>
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css" />
+                <link rel="stylesheet" href="/css/style.css" />
             </head>
             <body>
                 {html.children}
