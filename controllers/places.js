785a16454b2e06e1244d89a2709151c8b7ecfbcf
AndreTaq <andretaqian@gmail.com>
2023-06-05 21:32:22 -0700

    Delete functionality implemented

--- controllers/places.js
+++ controllers/places.js
@@ -16,8 +16,9 @@ router.get('/:id', (req, res) => {
     res.render('error404')
   }
    else {
-    res.render('places/show', {place: places[id]})
+    res.render('places/show', {place: places[id], id})
   }
+  
 })
 
 // POST 
@@ -35,6 +36,18 @@ router.post('/', (req, res) => {
   places.push(req.body)
   res.redirect('/places')
 })
+
+//Delete
+router.delete('/:id', (req, res) => {
+  let id = Number(req.params.id)
+  if(isNaN(id)){
+    res.render('error404')}
+      else if (!places[id]){
+        res.render('error404')
+      }else {
+        places.splice(id,1)
+  res.redirect('/places')
+}})
 
 //INDEX
 router.get('/', (req, res) => {
--- index.js
+++ index.js
@@ -1,12 +1,15 @@
+// Modules and Globals
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const methodOverride = require('method-override')
 
 
 // Middleware
 app.use(express.urlencoded({extended: true}))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
+app.use(methodOverride('_method'))
 app.engine('jsx', require('express-react-views').createEngine())
 app.use('/places', require('./controllers/places'))
 app.use(express.static('public'))
@@ -17,6 +20,7 @@ app.get('/', (req, res) => {
     // res.send('Hello World!') changed to
     res.render('home')
 })
+
 
 // 404 Page
 app.get('*', (req, res) => {
